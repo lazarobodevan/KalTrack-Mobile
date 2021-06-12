297b07018194001f60123fb61ae01e5dfbc5a5aa
Lázaro Bodevan <lazaro.maia@ufv.br>
2021-06-12 01:56:27 -0300

    bottom nav bar added

---
+++ KalTrack/KalTrack/Model/MealType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KalTrack.Model {
+    
+    /*-----This class works like an enum class
+      -----I decided to make this so I can return
+      -----a string related to my 'enum type' */
+    
+    public class MealType {
+        public string value;
+        private MealType(string value) {
+            this.value = value;
+        }
+
+        public static MealType Breakfast { get => new MealType("Café da manhã"); }
+        public static MealType Collation { get => new MealType("Colação"); }
+        public static MealType Lunch { get => new MealType("Colação"); }
+        public static MealType Aft_Coffee_break { get => new MealType("Café da tarde"); }
+        public static MealType Dinner { get => new MealType("Janta"); }
+        public static MealType Supper { get => new MealType("Ceia"); }
+    }
+}
--- KalTrack/KalTrack/ViewModel/HomePageVM.cs
+++ KalTrack/KalTrack/ViewModel/HomePageVM.cs
@@ -9,9 +9,17 @@ namespace KalTrack.ViewModel {
         public HomePageVM() {
             NavBack = new Command(Nav_Back);
             NavFourth = new Command(Nav_Fourth);
+            NavHome = new Command(Nav_Home);
+            NavChart = new Command(Nav_Chart);
+            NavCalendar = new Command(Nav_Calendar);
+            NavOptions = new Command(Nav_Options);
         }
         public ICommand NavBack { get; }
         public ICommand NavFourth { get; }
+        public ICommand NavHome { get; }
+        public ICommand NavChart { get; }
+        public ICommand NavCalendar { get; }
+        public ICommand NavOptions { get; }
 
         string monthText = DateTime.UtcNow.ToString("dd/MMMM");
 
@@ -25,6 +33,15 @@ namespace KalTrack.ViewModel {
 
         }
 
+        string txt = "Home";
+        public string lblTxt {
+            get => txt;
+            set {
+                txt = value;
+                OnPropertyChanged();
+            }
+        }
+
         void Nav_Back() {
             MonthText = DateTime.Parse(MonthText).AddMonths(-1).ToString("dd/MMMM");
         }
@@ -33,5 +50,17 @@ namespace KalTrack.ViewModel {
             MonthText = DateTime.Parse(MonthText).AddMonths(1).ToString("dd/MMMM");
         }
 
+        void Nav_Home() {
+            lblTxt = "Home";
+        }
+        void Nav_Chart() {
+            lblTxt = "Chart";
+        }
+        void Nav_Calendar() {
+            lblTxt = "Calendar";
+        }
+        void Nav_Options() {
+            lblTxt = "Options";
+        }
     }
 }
